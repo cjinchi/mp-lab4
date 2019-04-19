9294d0699231cfbce3c33b3ce6bb830fa4edd654
cjinchi <cjinchi@qq.com>
2019-04-19 11:41:09 +0800

    更新README

--- src/main/java/app/TotalOrderSorting.java
+++ src/main/java/app/TotalOrderSorting.java
@@ -1,11 +1,16 @@
 package app;
 
 import jdk.management.resource.internal.inst.SocketOutputStreamRMHooks;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 
 import java.io.IOException;
@@ -41,14 +46,20 @@ public class TotalOrderSorting {
         }
     }
 
-    public static void main(String[] args) {
-        Text text = new Text();
-        String str = "test  123.456789,金庸:12";
-        text.set(str);
-        StringTokenizer tokenizer = new StringTokenizer(text.toString().substring(0,text.find(",")));
-        String term = tokenizer.nextToken();
-        double times = Double.valueOf(tokenizer.nextToken());
-        System.out.println(term);
-        System.out.println(times);
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        //以下配置均参考自官方文档
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "total order sorting");
+        job.setJarByClass(TotalOrderSorting.class);
+        job.setMapperClass(TotalOrderSorting.TotalOrderSortingMapper.class);
+        job.setReducerClass(TotalOrderSorting.TotalOrderSortingReducer.class);
+        job.setPartitionerClass(TotalOrderSorting.InvertedIndexPartitioner.class);
+        job.setMapOutputKeyClass(DoubleWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
